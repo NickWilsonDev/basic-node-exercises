bf9c1b461b0b52cc95a47d98d2f17553ac88656c
Nick Wilson <logikfx@gmail.com>
2018-03-16 00:00:48 -0400

    finished first two exercises

--- dns_lookup.js
+++ dns_lookup.js
@@ -2,18 +2,18 @@
 
 
 const readline = require('readline');
-const fs = require('fs');
+const dns = require('dns');
 
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('filename: ', (answer) => {
-    fs.readFile(answer, function (err, data) {
+rl.question('Domain name: ', (name) => {
+    dns.lookup(name, (err, address) => {
         if (err)
             throw err;
-        console.log(data.toString().toUpperCase());
+        console.log('IP Address: ' + JSON.stringify(address));
     });
-    rl.close();
+    console.log("all done?");
 });
